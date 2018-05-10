bf043d2320fdd088282658fe63536e1a9132b620
Your Name <you@example.com>
2018-05-10 21:01:32 +0300

    page-clients, page-contact

---
+++ components/owl-carousel/owl-carousel__init.js
@@ -0,0 +1,14 @@
+/* INIT SCRIPTS FOR OWL CAROUSEL */
+
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    center: true,
+    margin:10,
+    nav:true,
+    items: 1,
+    loop: true,
+    autoplay: true,
+    nav: false
+  })
+});
