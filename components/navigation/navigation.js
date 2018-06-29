e9d2d6add6a0473544bbc9d976c9ef0f400fdee7
stanislavskiy <stanislavskiy.sanya@gmail.com>
2018-06-29 11:41:29 +0300

    fix

--- components/navigation/navigation.js
+++ components/navigation/navigation.js
@@ -1,4 +1,4 @@
-(function() {
+$(document).ready(function() {
   var hamburger = {
     navToggle: document.querySelector(".navigation__toggle"),
     nav: document.querySelector(".navigation__menu"),
@@ -11,9 +11,21 @@
   };
 
   hamburger.navToggle.addEventListener("click", function(e) {
-    hamburger.doToggle(e);
+    setTimeout(function() {
+      hamburger.doToggle(e);
+    }, 0);
   });
+
   hamburger.nav.addEventListener("click", function(e) {
-    hamburger.doToggle(e);
+    setTimeout(function() {
+      hamburger.doToggle(e);
+    }, 0);
   });
-})();
+
+  $(".navigation__menu").on("click", "a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+});
